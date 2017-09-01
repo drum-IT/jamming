00b8678a372fb6d0c2a54d152105ea48dbc7ba92
Miles <milesdpratt@gmail.com>
2017-08-31 20:59:34 -0400

    Add code to add tracks to playlist from search resuts. Add code to remove tracks from the playlist.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,9 +7,34 @@ import Playlist from '../Playlist/Playlist';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
     this.state = {
-      searchResults: [{name: 'test name',artist: 'test artist',album: 'test album', id: 'test id'}]
+      searchResults: [{name: 'test name search',artist: 'test artist search',album: 'test album search', id: '123er123r123r'}],
+      playlistName: 'playlist name',
+      playlistTracks: [{name: 'test name playlist',artist: 'test artist playlist',album: 'test album playlist', id: '12351234raewrf123r'}],
     }
+  }
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    for (let i = 0; i < tracks.length; i++) {
+      if (tracks[i].id === track.id) {
+        console.log('track already in list');
+        return;
+      }
+    }
+    tracks.push(track);
+    this.setState({
+      playlistTracks: tracks
+    });
+  }
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks.filter(playlistTrack => {
+      return playlistTrack.id !== track.id;
+    });
+    this.setState({
+      playlistTracks: tracks
+    })
   }
   render() {
     return (
@@ -18,8 +43,8 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
-            <Playlist />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist name={this.state.playlistName} tracks={this.state.playlistTracks} onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,12 +2,23 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
   renderAction(isRemoval) {
     if (isRemoval) {
-      return '-';
+      return <a className="Track-action" onClick={this.removeTrack}>-</a>;
     } else {
-      return '+';
+      return <a className="Track-action" onClick={this.addTrack}>+</a>;
     }
+  }
+  addTrack() {
+    this.props.onAdd(this.props.track);
+  }
+  removeTrack() {
+    this.props.onRemove(this.props.track);
   }
   render() {
     return (
@@ -16,7 +27,7 @@ class Track extends React.Component {
           <h3>{this.props.track.name}</h3>
           <p>{this.props.track.artist} | {this.props.track.album}</p>
         </div>
-        <a className="Track-action">{this.renderAction(false)}</a>
+        {this.renderAction(this.props.isRemoval)}
       </div>
     );
   }
