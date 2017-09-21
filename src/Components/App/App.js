fd15407a4843295da212c3fee318dc9668f0d2c3
Miles <milesdpratt@gmail.com>
2017-09-21 17:25:11 -0400

    Resolve issues with Spotify module preventing api calls. Update App,js to display search results.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
 
 class App extends React.Component {
   constructor(props) {
@@ -50,7 +51,11 @@ class App extends React.Component {
     })
   }
   search(term) {
-    console.log(term);
+    Spotify.search(term).then(tracks => {
+      this.setState({
+        searchResults: tracks
+      });
+    });
   }
   render() {
     return (
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -4,20 +4,49 @@ let accessToken = '';
 
 const Spotify = {
   getAccessToken() {
+    const tokenRegEx = /access_token=([^&]*)/;
+    const expireRegEx = /expires_in=([^&]*)/;
     if (accessToken !== '') {
-      return accessToken;
-    } else if (window.location.href.match(/access_token=([^&]*)/) !== null && window.location.href.match(/expires_in=([^&]*)/) !== null) {
-      let url = window.location.href;
-      accessToken = url.match(/access_token=([^&]*)/);
-      let expiresIn = url.match(/expires_in=([^&]*)/);
-      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      return new Promise(resolve =>
+        resolve(accessToken));
+    } else if (window.location.href.match(tokenRegEx) !== null && window.location.href.match(expireRegEx) !== null) {
+      accessToken = window.location.href.match(tokenRegEx)[1];
+      let expireTime = parseInt(window.location.href.match(expireRegEx)[1]);
+      window.setTimeout(() => accessToken = '', expireTime * 1000);
       window.history.pushState('Access Token', null, '/');
-    } else if (window.location.href.match(/access_token=([^&]*)/) === null && accessToken !== '') {
-      window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`
+      return new Promise(resolve =>
+        resolve(accessToken));
+    } else if (accessToken === '' && window.location.href.match(tokenRegEx) === null) {
+      window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
     }
-  }
+  },
   search(term) {
-    
+    return Spotify.getAccessToken().then(() => {
+      return fetch(
+        `https://api.spotify.com/v1/search?type=track&q=${term}`,
+        {
+          headers: {
+            'Authorization': 'Bearer ' + accessToken
+          }
+        }
+      ).then(response => {
+        return response.json();
+      }).then(jsonResponse => {
+        if (jsonResponse.tracks.length > 0) {
+          return jsonResponse.tracks.items.map(track => {
+            return {
+              id: track.id,
+              name: track.name,
+              artist: track.artists[0].name,
+              album: track.album.name,
+              uri: track.uri
+            }
+          });
+        } else {
+          return [];
+        }
+      });
+    })
   }
 };
 
